cf1f36b45b02f5719bf078637b18a1b67db96a6f
Souvik3798 <souvikbarua693@gmail.com>
2023-10-08 21:50:19 +0530

    limit

--- app/Filament/Resources/IternityTemplateResource.php
+++ app/Filament/Resources/IternityTemplateResource.php
@@ -50,7 +50,17 @@ class IternityTemplateResource extends Resource
                 TextColumn::make('Title')
                 ->label('Title'),
                 TextColumn::make('Description')
-                ->limit(50),
+                ->limit(50)
+                ->tooltip(function (TextColumn $column): ?string {
+                    $state = $column->getState();
+
+                    if (strlen($state) <= $column->getCharacterLimit()) {
+                        return null;
+                    }
+
+                    // Only render the tooltip if the column content exceeds the length limit.
+                    return $state;
+                }),
                 TextColumn::make('Specialties')
                 ->wrap(),
                 TextColumn::make('locationCovered')
